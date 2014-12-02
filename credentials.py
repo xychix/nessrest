10cfab4b41d335e586ecc51208e9eba7c841a39f
Matt Everson <meverson@tenable.com>
2014-12-02 00:07:55 -0600

    Public key SSH credential definition, requiring a previously-uploaded file name.

--- credentials.py
+++ credentials.py
@@ -12,19 +12,25 @@ class SSH:
     category = "Host"
     name = "SSH"
 
-    def __init__(self, username, password, auth_method="password", elevate_privileges_with="Nothing", escalation_account="", escalation_password=""):
+    def __init__(self, username, password="", auth_method="password", elevate_privileges_with="Nothing", escalation_account="", escalation_password="", private_key_filename="", private_key_passphrase=""):
         if auth_method not in ("password", "certificate", "Kerberos", "public key"):
             raise ValueError('auth_method not valid')
 
-        self.username = username
-        self.password = password
         self.auth_method = auth_method
-        if elevate_privileges_with == "Cisco 'enable'":
-            self.escalation_password = escalation_password
-        if elevate_privileges_with == "sudo":
-            self.escalation_account = escalation_account
-            self.escalation_password = escalation_password
         self.elevate_privileges_with = elevate_privileges_with
+        if auth_method == "public key":
+            self.username = username
+            self.private_key = private_key_filename
+            self.private_key_passphrase = private_key_passphrase
+
+        if auth_method == "password":
+            self.username = username
+            self.password = password
+            if elevate_privileges_with == "Cisco 'enable'":
+                self.escalation_password = escalation_password
+                if elevate_privileges_with == "sudo":
+                    self.escalation_account = escalation_account
+                    self.escalation_password = escalation_password
 
 class Salesforce:
     category = "Cloud Services"

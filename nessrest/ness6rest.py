7c5655c03e75dd164ecfeb185bd5041da5651ac4
Chad Streck <cstreck@tenable.com>
2015-03-06 09:35:25 -0600

    Decode json when text has been returned.

--- nessrest/ness6rest.py
+++ nessrest/ness6rest.py
@@ -165,7 +165,7 @@ class Scanner(object):
             req = requests.request(method, url, data=payload, files=files,
                                    verify=verify, headers=headers)
 
-            if not download:
+            if not download and req.text:
                 self.res = req.json()
 
             if req.status_code != 200:
